3939425f12eae6a8b83034f481639d0a3bd5ae75
T. Scott Barnes <barnes.t.scott@gmail.com>
2014-09-30 13:17:23 -0400

    Set up Grunt and NPM

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    less: {
+      development: {
+        options: {
+          paths: ['less/']
+        },
+        files: [{
+          src: ['less/core.less'],
+          dest: 'public/css/core.css'
+        }]
+      }
+    },
+    copyto: {
+      development: {
+        files: [{
+          cwd: 'js',
+          src: ['**/*'],
+          dest: 'public/js/'
+        }, {
+          cwd: 'lib',
+          src: ['**/*'],
+          dest: 'public/lib/'
+        }],
+        options: {
+          ignore: ['**/src{,/**/*}']
+        }
+      }
+    },
+    mkdir: {
+      development: {
+        options: {
+          create: ['public/css', 'public/js', 'public/lib']
+        },
+      },
+    },
+    clean: {
+      build: ['public/css', 'public/js', 'public/lib']
+    },
+    watch: {
+      css: {
+        files: ['less/**'],
+        tasks: ['less:development']
+      },
+      js: {
+        files: ['js/**', 'lib/**'],
+        tasks: ['copyto:development']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-copy-to');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Default task.
+  grunt.registerTask('default', ['clean', 'mkdir', 'less', 'copyto']);
+
+};
