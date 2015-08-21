3bc2533936faeaeee238bee3b8fd22a92d9ee5fb
T. Scott Barnes <scott@awesomebarnes.com>
2015-08-20 23:52:27 -0400

    Added Travis-CI config

--- awesomebarnes/settings.py
+++ awesomebarnes/settings.py
@@ -86,6 +86,7 @@ INSTALLED_APPS = (
     'mptt',
     'easy_thumbnails',
     'adminsortable',
+    'django_nose',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -118,7 +119,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'awesomebarnes.wsgi.application'
-
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
